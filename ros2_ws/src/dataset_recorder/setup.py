0690539409ba3984054a8228ee9d414899496cbe
ssapsu <hans324oh@gmail.com>
2025-04-30 21:45:11 +0900

    데이터셋 수집 ros2 패키지

---
+++ ros2_ws/src/dataset_recorder/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+package_name = "dataset_recorder"
+
+setup(
+    name=package_name,
+    version="0.1.0",
+    packages=[package_name],
+    install_requires=["setuptools"],
+    zip_safe=True,
+    maintainer="ssapsu",
+    maintainer_email="hans324oh@gmail.com",
+    description="Record specified topics to dataset files",
+    license="Apache License 2.0",
+    tests_require=["pytest"],
+    entry_points={
+        "console_scripts": [
+            "recorder = dataset_recorder.recorder:main",
+        ],
+    },
+)
